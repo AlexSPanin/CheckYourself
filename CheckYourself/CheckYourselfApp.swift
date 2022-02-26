3f0b3ede0a3ffeb4625767cac3c20bad056ed6b3
Alexander Panin <panin1970@gmail.com>
2022-02-26 09:46:36 +0300

    Initial Commit

---
+++ CheckYourself/CheckYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckYourselfApp.swift
+//  CheckYourself
+//
+//  Created by Александр Панин on 26.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
